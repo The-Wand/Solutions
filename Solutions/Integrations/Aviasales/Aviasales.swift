2d57b1f4ad56188fe49984105f502e5437977355
Alex Kozin <al@el-machine.com>
2025-06-06 16:28:14 +0700

    Add Aviasales API concept

---
+++ Solutions/Integrations/Aviasales/Aviasales.swift
@@ -0,0 +1,19 @@
+//
+//  Aviasales.swift
+//  Solutions
+//
+//  Created by al on 6/6/25.
+//
+
+import Foundation
+
+struct Aviasales {
+    
+    enum Ticket: Orderable {
+        
+        case oneWay
+        case roundTrip
+        
+    }
+    
+}
